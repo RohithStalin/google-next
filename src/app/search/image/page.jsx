0df98b23cd3f7a0f9dd188ce87c3518307e505ee
RohithStalin <stalinrohith219@gmail.com>
2024-07-05 12:10:52 +0530

    added image result page

--- src/app/search/image/page.jsx
+++ src/app/search/image/page.jsx
@@ -1,11 +1,32 @@
 import React from "react";
-
-const ImageSearchPage = () => {
-  return (
-    <div>
-      <h1 className="text-red-500">Image search page</h1>
-    </div>
+import Link from "next/link";
+import ImageSearchResults from "@/components/ImageSearchResults";
+const ImageSearchPage = async ({ searchParams }) => {
+  const response = await fetch(
+    `https://www.googleapis.com/customsearch/v1?key=${process.env.API_KEY}&cx=${process.env.CONTEXT_API_KEY}&q=${searchParams.searchTerm}&searchType=image`
   );
+  if (!response.ok) throw new Error("Something Went Wrong");
+  const data = await response.json();
+  const results = data.items;
+
+  if (!results) {
+    return (
+      <div className="flex flex-col justify-center items-center pt-10">
+        <h1 className="text-3xl mb-4">
+          No Results Found for {searchParams.searchTerm}
+        </h1>
+        <p className="text-lg">
+          Try Searching the web or images for something else
+          <Link href="/" className="text-blue-500">
+            {" "}
+            Home
+          </Link>
+        </p>
+      </div>
+    );
+  }
+
+  return <div>{results && <ImageSearchResults results={data} />}</div>;
 };
 
 export default ImageSearchPage;
